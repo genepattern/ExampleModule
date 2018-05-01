cee0f2be16dfd436b1e550f6b862019187afd7c3
edjuaro <edjuaro@gmail.com>
2018-05-01 15:11:18 -0700

    gpunits passing

---
+++ src/ABasicModule_functions.py
@@ -0,0 +1,3 @@
+# Here is where I'd put my functions, if I had any!
+def empty_function():
+    return
--- src/run_client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Hello World! We will be runnig the following command
-echo ./gdc-client
-sudo chmod +x gdc-client

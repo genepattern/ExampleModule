5bd4a8fe9eb8583a90d64fadb6e236fd628f0f39
bahill <bhill@broadinstitute.org>
2021-05-17 15:27:42 -0400

    Set python3 shebang

--- src/ExampleModule.py
+++ src/ExampleModule.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from ExampleModule_functions import *
 import argparse
 import humanfriendly
--- src/ExampleModule_functions.py
+++ src/ExampleModule_functions.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Here is where I'd put my functions, if I had any!
 def empty_function():
     return
